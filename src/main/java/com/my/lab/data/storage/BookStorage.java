61009077f46d257e5be6464da428b3102b4047fc
harumamburu <nikolay.kovalev92@gmail.com>
2016-04-21 18:53:15 +0300

    added memory (HashMap) storage for book entities

---
+++ src/main/java/com/my/lab/data/storage/BookStorage.java
@@ -0,0 +1,29 @@
+package com.my.lab.data.storage;
+
+import com.my.lab.entity.Book;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BookStorage {
+
+    private static final AtomicInteger increment = new AtomicInteger();
+    private static final Map<Integer, Book> books = new HashMap<>();
+
+    public static Book addBook(Book book) {
+        if (book.getId() == null) {
+            book.setId(increment.incrementAndGet());
+        }
+        books.put(book.getId(), book);
+        return book;
+    }
+
+    public static Book getBook(Integer id) {
+        return books.get(id);
+    }
+
+    public static  Book deleteBook(Integer id) {
+        return books.remove(id);
+    }
+}
--- src/main/java/com/my/lab/resource/BookResource.java
+++ src/main/java/com/my/lab/resource/BookResource.java
@@ -1,58 +1,50 @@
 package com.my.lab.resource;
 
+import com.my.lab.data.storage.BookStorage;
 import com.my.lab.entity.Book;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
-import java.util.Map;
 
 @Path("/book")
 public class BookResource {
-
-    private static final Map<Integer, Book> books = new HashMap<>();
 
     private static final String PARAM_BOOK_ID = "bookId";
 
     @GET
     @Produces({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
     public Response getBookById(@QueryParam(PARAM_BOOK_ID) String bookId) {
-        try {
-            if (bookId.equals("null") || bookId == null) {
-                throw new BadRequestException(PARAM_BOOK_ID + " can't be null!");
-            }
-
-            Book book = books.get(Integer.valueOf(bookId));
-            if (book == null) {
-                return Response.serverError().entity("No book found by id = " + bookId).build();
-            }
-            return Response.ok(book).build();
-        } catch (NullPointerException exc) {
-            return Response.serverError().entity("Oh no! Books storage weren't initialized!").build();
+        if (bookId == null || bookId.equals("null")) {
+            throw new BadRequestException(PARAM_BOOK_ID + " can't be null!");
         }
+
+        Book book = BookStorage.getBook(Integer.valueOf(bookId));
+        if (book == null) {
+            return Response.serverError().entity("No book found by id = " + bookId).build();
+        }
+        return Response.ok(book).build();
     }
 
     @POST
     @Consumes({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
     @Produces({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
     public Response saveBook(Book book) {
-        return Response.ok().entity(book).build();
+        return Response.ok().entity(BookStorage.addBook(book)).build();
     }
 
     @PUT
-    public String saveOrUpdateBook(String book) {
-        return null;//saveBook(book);
+    @Consumes({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
+    @Produces({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
+    public Response saveOrUpdateBook(Book book) {
+        return saveBook(book);
     }
 
     @DELETE
+    @Produces({/*MediaType.APPLICATION_XML, */MediaType.APPLICATION_JSON})
     public String deleteBook(@QueryParam(PARAM_BOOK_ID) String bookId) {
-        try {
-            Book bookObj = books.remove(Integer.valueOf(bookId));
-            return bookObj == null
-                    ? "No book found with id = " + bookId : bookObj.toString() + "\r\nDeleted";
-        } catch (NullPointerException exc) {
-            return "Oh no! Books storage weren't initialized!";
-        }
+        Book book = BookStorage.deleteBook(Integer.valueOf(bookId));
+        return book == null
+                ? "No book found with id = " + bookId : book.toString() + "\r\nDeleted";
     }
 }
