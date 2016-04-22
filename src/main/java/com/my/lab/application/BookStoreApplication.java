2632bba1a7a29c96c0d7ccc7a6b56b94e1677b1c
harumamburu <nikolay.kovalev92@gmail.com>
2016-04-22 18:00:51 +0300

    Added swagger initialization and config

--- src/main/java/com/my/lab/application/BookStoreApplication.java
+++ src/main/java/com/my/lab/application/BookStoreApplication.java
@@ -1,5 +1,6 @@
 package com.my.lab.application;
 
+import io.swagger.jaxrs.config.BeanConfig;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -8,7 +9,15 @@ import javax.ws.rs.ApplicationPath;
 public class BookStoreApplication extends ResourceConfig {
 
     public BookStoreApplication() {
-        packages("com.my.lab");
+        BeanConfig swaggerConfig = new BeanConfig();
+        swaggerConfig.setSchemes(new String[]{"http", "https"});
+        swaggerConfig.setHost("localhost:8080");
+        swaggerConfig.setBasePath("/bookstore/api");
+        swaggerConfig.setResourcePackage("com.my.lab");
+        swaggerConfig.setScan(true);
+
+        packages("com.my.lab","io.swagger.jaxrs.json", "io.swagger.jaxrs.listing",
+                "com.fasterxml.jackson.jaxrs");
         register(CustomJacksonDateMappingProvider.class);
     }
 }
