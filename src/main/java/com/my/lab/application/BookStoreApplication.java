21de862fbf8b92203e77bce57480b63bfe6e21eb
harumamburu <nikolay.kovalev92@gmail.com>
2016-04-25 18:36:38 +0300

    done with swagger configuration

--- src/main/java/com/my/lab/application/BookStoreApplication.java
+++ src/main/java/com/my/lab/application/BookStoreApplication.java
@@ -1,6 +1,7 @@
 package com.my.lab.application;
 
 import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.models.Info;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -9,14 +10,18 @@ import javax.ws.rs.ApplicationPath;
 public class BookStoreApplication extends ResourceConfig {
 
     public BookStoreApplication() {
+        initSwagger();
+        packages("com.my.lab","io.swagger.jaxrs.json", "io.swagger.jaxrs.listing");
+        register(CustomJacksonDateMappingProvider.class);
+    }
+
+    private void initSwagger() {
         BeanConfig swaggerConfig = new BeanConfig();
         swaggerConfig.setSchemes(new String[]{"http", "https"});
         swaggerConfig.setHost("localhost:8080");
-        swaggerConfig.setBasePath("/bookstore/api");
+        swaggerConfig.setBasePath("/jaxrsws/bookstore/api");
         swaggerConfig.setResourcePackage("com.my.lab");
+        swaggerConfig.setInfo(new Info().title("Book store web API"));
         swaggerConfig.setScan(true);
-
-        packages("com.my.lab","io.swagger.jaxrs.json", "io.swagger.jaxrs.listing");
-        register(CustomJacksonDateMappingProvider.class);
     }
 }
