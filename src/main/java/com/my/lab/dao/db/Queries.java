2ac7ea146f2abcd2548c9d1bf5d8e676cbce610c
harumamburu <nikolay.kovalev92@gmail.com>
2016-05-02 12:31:54 +0300

    fixed broken queries by changing table and column names with class and field names

--- src/main/java/com/my/lab/dao/db/Queries.java
+++ src/main/java/com/my/lab/dao/db/Queries.java
@@ -1,26 +1,20 @@
 package com.my.lab.dao.db;
 
-import com.my.lab.business.Constants;
-
 public interface Queries {
 
     String QUERY_PARAM_ID = "id";
 
     
     String BOOK_DELETE_BYID_QUERYNAME = "Book.DeleteById";
-    String BOOK_DELETE_BYID_QUERY = "delete " + Constants.BOOK_TABLE_NAME +
-            " b where b." + Constants.BOOK_COLUMN_ID + " = :" + QUERY_PARAM_ID;
+    String BOOK_DELETE_BYID_QUERY = "delete Book b where b.bookId = :" + QUERY_PARAM_ID;
     
     String BOOK_CHECK_BYID_QUERYNAME = "Book.CheckById";
-    String BOOK_CHECK_BYID_QUERY = "select 1 from " + Constants.BOOK_TABLE_NAME +
-            " b where b." + Constants.BOOK_COLUMN_ID + " = :" + QUERY_PARAM_ID;
+    String BOOK_CHECK_BYID_QUERY = "select 1 from Book b where b.bookId = :" + QUERY_PARAM_ID;
 
     
     String AUTHOR_DELETE_BYID_QUERYNAME = "Author.DeleteById";
-    String AUTHOR_DELETE_BYID_QUERY = "delete " + Constants.AUTHOR_TABLE_NAME +
-            " a where a." + Constants.AUTHOR_COLUMN_ID + " = :" + QUERY_PARAM_ID;
+    String AUTHOR_DELETE_BYID_QUERY = "delete Author a where a.authorId = :" + QUERY_PARAM_ID;
     
     String AUTHOR_CHECK_BYID_QUERYNAME = "Author.CheckById";
-    String AUTHOR_CHECK_BYID_QUERY = "select 1 from  " + Constants.AUTHOR_TABLE_NAME +
-            " a where a." + Constants.AUTHOR_COLUMN_ID + " = :" + QUERY_PARAM_ID;
+    String AUTHOR_CHECK_BYID_QUERY = "select 1 from  Author a where a.authorId = :" + QUERY_PARAM_ID;
 }
