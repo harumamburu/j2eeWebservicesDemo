182d5cbd7e30d5dca537ee7edaab159977a1d3df
harumamburu <nikolay.kovalev92@gmail.com>
2016-05-27 10:51:04 +0300

    fill in web entities

--- src/main/java/com/my/lab/web/entity/Author.java
+++ src/main/java/com/my/lab/web/entity/Author.java
@@ -1,4 +1,48 @@
 package com.my.lab.web.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.my.lab.web.setting.json.deserialization.BirthDateDeserializer;
+import com.my.lab.web.setting.json.deserialization.BirthDateSerializer;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
 public class Author {
+
+    @NotNull
+    private Integer authorId;
+    @NotNull
+    private String name;
+    @JsonDeserialize(using = BirthDateDeserializer.class)
+    @JsonSerialize(using = BirthDateSerializer.class)
+    private Date birth;
+
+    public Author() {
+        // Default empty constructor JSON data binding
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
 }
--- src/main/java/com/my/lab/web/entity/Book.java
+++ src/main/java/com/my/lab/web/entity/Book.java
@@ -1,4 +1,74 @@
 package com.my.lab.web.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.my.lab.core.converters.dao.enumeration.Genre;
+import com.my.lab.web.setting.json.deserialization.PublishingDateDeserializer;
+import com.my.lab.web.setting.json.deserialization.PublishingDateSerializer;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
 public class Book {
+
+    @NotNull
+    private Integer bookId;
+    @NotNull
+    private String name;
+    @NotNull
+    private List<Author> authors;
+    private List<Genre> genres;
+    @JsonDeserialize(using = PublishingDateDeserializer.class)
+    @JsonSerialize(using = PublishingDateSerializer.class)
+    private Date written;
+
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public Book() {
+        // Default empty constructor for JSON data binding
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public Date getWritten() {
+        return written;
+    }
+
+    public void setWritten(Date written) {
+        this.written = written;
+    }
 }
