b264548385f381df0d09b27cd541dca91dd29641
harumamburu <nikolay.kovalev92@gmail.com>
2016-05-27 10:25:40 +0300

    fill DTOs with fields and access properties

--- src/main/java/com/my/lab/core/dto/AuthorDTO.java
+++ src/main/java/com/my/lab/core/dto/AuthorDTO.java
@@ -1,14 +1,36 @@
 package com.my.lab.core.dto;
 
+import java.util.Date;
+
 public class AuthorDTO implements DTO {
+
+    private Integer authorId;
+    private String name;
+    private Date birth;
 
     @Override
     public Integer getId() {
-        return null;
+        return authorId;
     }
 
     @Override
     public void setId(Integer id) {
+        this.authorId = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
     }
 }
--- src/main/java/com/my/lab/core/dto/BookDTO.java
+++ src/main/java/com/my/lab/core/dto/BookDTO.java
@@ -1,14 +1,57 @@
 package com.my.lab.core.dto;
 
+import com.my.lab.core.converters.dao.enumeration.Genre;
+
+import java.util.Date;
+import java.util.List;
+
 public class BookDTO implements DTO {
+
+    private Integer bookId;
+    private String name;
+    private List<AuthorDTO> authors;
+    private List<Genre> genres;
+    private Date written;
 
     @Override
     public Integer getId() {
-        return null;
+        return bookId;
     }
 
     @Override
     public void setId(Integer id) {
+        bookId = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<AuthorDTO> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<AuthorDTO> authors) {
+        this.authors = authors;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public Date getWritten() {
+        return written;
+    }
+
+    public void setWritten(Date written) {
+        this.written = written;
     }
 }
