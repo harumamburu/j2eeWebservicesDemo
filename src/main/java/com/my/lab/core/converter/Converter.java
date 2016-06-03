605c1ebda70d46260fc4825025d93b7b6ab316d4
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-03 16:08:23 +0300

    safely delete unused converter interface
    Converters were replaced by mapstruct mappers

--- src/main/java/com/my/lab/core/converter/Converter.java
+++
@@ -1,9 +0,0 @@
-package com.my.lab.core.converter;
-
-import com.my.lab.core.dto.DTO;
-
-public interface Converter <T extends DTO, K> {
-
-    T convertToDTO(K entity);
-    K convertFromDTO(T dto);
-}
