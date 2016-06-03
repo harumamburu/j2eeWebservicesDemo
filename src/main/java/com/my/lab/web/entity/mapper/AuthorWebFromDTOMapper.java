3321bfb5dd93946344faf2b7a42e653b5dba284d
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-03 14:21:56 +0300

    add mapstruct from DTO mappers for web entities

---
+++ src/main/java/com/my/lab/web/entity/mapper/AuthorWebFromDTOMapper.java
@@ -0,0 +1,14 @@
+package com.my.lab.web.entity.mapper;
+
+import com.my.lab.core.dto.AuthorDTO;
+import com.my.lab.web.entity.AuthorWebEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AuthorWebFromDTOMapper {
+
+    AuthorWebFromDTOMapper INSTANCE = Mappers.getMapper(AuthorWebFromDTOMapper.class);
+
+    AuthorWebEntity authorFromDTO(AuthorDTO authorDTO);
+}
---
+++ src/main/java/com/my/lab/web/entity/mapper/BookWebFromDTOMapper.java
@@ -0,0 +1,14 @@
+package com.my.lab.web.entity.mapper;
+
+import com.my.lab.core.dto.BookDTO;
+import com.my.lab.web.entity.BookWebEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = AuthorWebFromDTOMapper.class)
+public interface BookWebFromDTOMapper {
+
+    BookWebFromDTOMapper INSTANCE = Mappers.getMapper(BookWebFromDTOMapper.class);
+
+    BookWebEntity authorFromDTO(BookDTO bookDTO);
+}
