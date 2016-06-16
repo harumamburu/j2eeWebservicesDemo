ba2b690a1fcd3c655c214dc1c2584a39fd615de7
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-16 16:03:15 +0300

    add an abstract service class to encapsulate implementation details
    abstract class adds protected "routine" methods to be implemented in concrete sub-classes and exposes an interface's contract with basic try/catches being already implemented

---
+++ src/main/java/com/my/lab/web/service/AbstractService.java
@@ -0,0 +1,63 @@
+package com.my.lab.web.service;
+
+import com.my.lab.core.adapter.exception.AlreadyExistsException;
+import com.my.lab.core.adapter.exception.DataPersistenceException;
+import com.my.lab.core.adapter.exception.NotAllowedException;
+import com.my.lab.web.entity.WebEntity;
+import com.my.lab.web.error.EntityAlreadyExistsException;
+import com.my.lab.web.error.EntityMisformattedException;
+import com.my.lab.web.error.EntityNotFoundException;
+import com.my.lab.web.error.InternalException;
+
+abstract class AbstractService<T extends WebEntity> implements Service<T> {
+
+    public T onGet(Integer id) {
+        try {
+            return onGetRoutine(id);
+        }  catch (DataPersistenceException exc) {
+            throw new InternalException(exc);
+        }
+    }
+
+    protected abstract T onGetRoutine(Integer id) throws DataPersistenceException;
+
+    public T onPost(T entity) {
+        try {
+            return onPostRoutine(entity);
+        } catch (AlreadyExistsException exc) {
+            throw new EntityAlreadyExistsException(exc.getMessage(), exc);
+        } catch(NotAllowedException exc) {
+            throw new EntityMisformattedException(exc);
+        } catch (DataPersistenceException exc) {
+            throw new InternalException(exc);
+        }
+    }
+
+    protected abstract T onPostRoutine(T entity) throws DataPersistenceException;
+
+    public T onPut(T entity) {
+        try {
+            return onPutRoutine(entity);
+        } catch (DataPersistenceException exc) {
+            throw new InternalException(exc);
+        }
+    }
+
+    protected abstract T onPutRoutine(T entity) throws DataPersistenceException;
+
+    public T onDelete(Integer id) {
+        try {
+            return onDeleteRoutine(id);
+        } catch (DataPersistenceException exc) {
+            throw new InternalException(exc);
+        }
+    }
+
+    protected abstract T onDeleteRoutine(Integer id) throws DataPersistenceException;
+
+    protected void checkEntityNotNull(WebEntity entity, Integer id) {
+        if (entity == null) {
+            throw new EntityNotFoundException("No entity found by id = " + id);
+        }
+    }
+}
--- src/main/java/com/my/lab/web/service/BookService.java
+++ src/main/java/com/my/lab/web/service/BookService.java
@@ -2,16 +2,10 @@ package com.my.lab.web.service;
 
 import com.my.lab.core.adapter.BookAdapter;
 import com.my.lab.core.adapter.exception.DataPersistenceException;
-import com.my.lab.core.adapter.exception.NotAllowedException;
 import com.my.lab.core.dto.BookDTO;
-import com.my.lab.core.adapter.exception.AlreadyExistsException;
 import com.my.lab.web.entity.BookWebEntity;
 import com.my.lab.web.entity.mapper.frommapper.BookWebFromDTOMapper;
 import com.my.lab.web.entity.mapper.tomapper.BookWebToDTOMapper;
-import com.my.lab.web.error.EntityAlreadyExistsException;
-import com.my.lab.web.error.EntityMisformattedException;
-import com.my.lab.web.error.EntityNotFoundException;
-import com.my.lab.web.error.InternalException;
 
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -19,53 +13,33 @@ import javax.ejb.Stateless;
 
 @Stateless
 @LocalBean
-public class BookService implements Service<BookWebEntity> {
+public class BookService extends AbstractService<BookWebEntity> {
 
     @EJB
     private BookAdapter bookAdapter;
 
     @Override
-    public BookWebEntity onGet(Integer id) {
-        try {
-            BookWebEntity book = bookFromDTO(bookAdapter.getEntity(id));
-            checkBookNotNull(book, id);
-            return book;
-        } catch (DataPersistenceException exc) {
-            throw new InternalException(exc);
-        }
+    protected  BookWebEntity onGetRoutine(Integer id) throws DataPersistenceException {
+        BookWebEntity book = bookFromDTO(bookAdapter.getEntity(id));
+        checkEntityNotNull(book, id);
+        return book;
     }
 
     @Override
-    public BookWebEntity onPost(BookWebEntity book) {
-        try {
-            return bookFromDTO(bookAdapter.saveEntity(bookToDTO(book)));
-        } catch (AlreadyExistsException exc) {
-            throw new EntityAlreadyExistsException(exc.getMessage(), exc);
-        } catch(NotAllowedException exc) {
-            throw new EntityMisformattedException(exc);
-        } catch (DataPersistenceException exc) {
-            throw new InternalException(exc);
-        }
+    protected  BookWebEntity onPostRoutine(BookWebEntity book) throws DataPersistenceException {
+        return bookFromDTO(bookAdapter.saveEntity(bookToDTO(book)));
     }
 
     @Override
-    public BookWebEntity onPut(BookWebEntity book) {
-        try {
-            return bookFromDTO(bookAdapter.updateEntity(bookToDTO(book)));
-        } catch (DataPersistenceException exc) {
-            throw new InternalException(exc);
-        }
+    protected  BookWebEntity onPutRoutine(BookWebEntity book) throws DataPersistenceException {
+        return bookFromDTO(bookAdapter.updateEntity(bookToDTO(book)));
     }
 
     @Override
-    public BookWebEntity onDelete(Integer id) {
-        try {
-            BookWebEntity book = bookFromDTO(bookAdapter.deleteEntity(id));
-            checkBookNotNull(book, id);
-            return book;
-        } catch (DataPersistenceException exc) {
-            throw new InternalException(exc);
-        }
+    protected  BookWebEntity onDeleteRoutine(Integer id) throws DataPersistenceException {
+        BookWebEntity book = bookFromDTO(bookAdapter.deleteEntity(id));
+        checkEntityNotNull(book, id);
+        return book;
     }
 
     private BookDTO bookToDTO(BookWebEntity book) {
@@ -74,11 +48,5 @@ public class BookService implements Service<BookWebEntity> {
 
     private BookWebEntity bookFromDTO(BookDTO bookDTO) {
         return BookWebFromDTOMapper.INSTANCE.bookFromDTO(bookDTO);
-    }
-
-    private void checkBookNotNull(BookWebEntity book, Integer id) {
-        if (book == null) {
-            throw new EntityNotFoundException("No book found by id = " + id);
-        }
     }
 }
