47a07dcd2b986d740b69105fa53a4f017a2ccc96
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-21 14:15:20 +0300

    add documentation and comments for a custom ID generator

--- src/main/java/com/my/lab/dao/entity/identifier/IdCheckingSequenceStyleGenerator.java
+++ src/main/java/com/my/lab/dao/entity/identifier/IdCheckingSequenceStyleGenerator.java
@@ -12,28 +12,50 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * An extension for {@link SequenceStyleGenerator} which allows mixed-type ID generation
+ * (an ID won't be generated if it was provided by user)
+ */
 public class IdCheckingSequenceStyleGenerator extends SequenceStyleGenerator {
 
+    /**
+     * keeps user-provided IDs for different entities types
+     */
     private static Map<Class<?>, List<Integer>> idsMap = new ConcurrentHashMap<>(8, 0.9f, 1);
 
+    /**
+     *Checks if a provided entity is an instance of {@link Identifiable} and if it has an id,
+     * generates one if it's not the case
+     * @param session see {@link SessionImplementor}
+     * @param object an entity object
+     * @return {@link Serializable} ID object, either generated or provided by user
+     * @throws HibernateException
+     */
     @Override
     public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
         if (object instanceof Identifiable) {
             Identifiable identifiable = (Identifiable) object;
             Integer id = identifiable.getId();
             if (id != null) {
+                // get an ids list for an entity's class and save a provided ID
                 List<Integer> ids = idsMap.getOrDefault(identifiable.getClass(), new ArrayList<>());
                 ids.add(id);
                 idsMap.put(identifiable.getClass(), ids);
                 return id;
             }
+            // get the last generated ID value holder
             IntegralDataTypeHolder dtHolder = super.getOptimizer().getLastSourceValue();
+            // just a null-check for the very first generation
             if (dtHolder != null) {
-                Integer previous = Integer.valueOf(dtHolder.makeValue().intValue());
+                // get the value of ID to be generated (the last generated + 1)
+                Integer next = Integer.valueOf(dtHolder.makeValue().intValue() + 1);
                 synchronized (this.getClass()) {
+                    // check if such ID value was provided by user for this entity type
+                    // remove it from the list (it won't be needed any more) if it was
+                    // and blank-generate it (related sequence will be queried)
                     List<Integer> ids = idsMap.get(identifiable.getClass());
-                    if (ids != null && ids.contains(++previous)) {
-                        ids.remove(previous);
+                    if (ids != null && ids.contains(next)) {
+                        ids.remove(next);
                         super.generate(session, object);
                     }
                 }
