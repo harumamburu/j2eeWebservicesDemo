d755c7de5b279d04cdc5d59c94e4e496ca7067da
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-23 12:30:07 +0300

    add a hint for a date format checking regexp fix

--- src/main/java/com/my/lab/web/entity/format/DateFormats.java
+++ src/main/java/com/my/lab/web/entity/format/DateFormats.java
@@ -20,7 +20,7 @@ public enum DateFormats {
     public Date fromSting(String date) {
         try {
             // TODO: fix \\d4 leading to birth dates being misparsed
-            Matcher matcher = Pattern.compile("\\d{4}").matcher(date);//([A-Za-z]+ \d{1,2}, \d{4}|\d{4})
+            Matcher matcher = Pattern.compile("\\d{4}").matcher(date);//([A-Za-z]+ \\d{1,2}, \\d{4}|\\d{4})
             return matcher.find() ? format.parse(matcher.group()) : null;
         } catch (ParseException | NullPointerException exc) {
             return null;
