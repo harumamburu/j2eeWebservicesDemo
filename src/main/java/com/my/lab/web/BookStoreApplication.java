f732ef1b830e6f7adfb860a2331c640ffdab20c3
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-23 12:03:31 +0300

    make jax-rs application environment agnostic
    by extending jax-rs Application class instead of jersey-specific class

--- src/main/java/com/my/lab/web/BookStoreApplication.java
+++ src/main/java/com/my/lab/web/BookStoreApplication.java
@@ -1,21 +1,41 @@
 package com.my.lab.web;
 
+import com.my.lab.web.error.mapper.ErrorToResponseMapper;
+import com.my.lab.web.resource.AuthorResource;
+import com.my.lab.web.resource.BookResource;
 import com.my.lab.web.setting.json.CustomJacksonDateMappingProvider;
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.models.Info;
-import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-// TODO: make deployment-agnostic (extend application)
 @ApplicationPath("/bookstore/api")
-public class BookStoreApplication extends ResourceConfig {
-
-    public BookStoreApplication() {
+public class BookStoreApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
         initSwagger();
-        packages("com.my.lab","io.swagger.jaxrs.json", "io.swagger.jaxrs.listing");
-        register(CustomJacksonDateMappingProvider.class);
-        property("jersey.config.disableMoxyJson", true);
+
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(BookResource.class);
+        classes.add(AuthorResource.class);
+        classes.add(CustomJacksonDateMappingProvider.class);
+        classes.add(ErrorToResponseMapper.class);
+        classes.add(io.swagger.jaxrs.json.JacksonJsonProvider.class);
+        classes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        classes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return classes;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        final Map<String, Object> properties = new HashMap<>();
+        properties.put("jersey.config.disableMoxyJson", true);
+        return  properties;
     }
 
     private void initSwagger() {
